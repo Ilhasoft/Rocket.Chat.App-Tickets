a546cbc940966f9aead3c1860e893b1140a84366
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2020-07-22 18:47:09 -0300

    agent message webhook done and some tweaks on secret.check endpoint

--- app/remote/hooks/rapidpro/RapidProWebhook.ts
+++ app/remote/hooks/rapidpro/RapidProWebhook.ts
@@ -1,15 +1,18 @@
-import { IHttp } from '@rocket.chat/apps-engine/definition/accessors';
+import { IHttp, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IMessageAttachment } from '@rocket.chat/apps-engine/definition/messages';
 
 import IWebhookRepository from '../../../data/hooks/rapidpro/IWebhookRepository';
 import Room from '../../../domain/Room';
+import { RC_SERVER_URL } from '../../../settings/Constants';
 
 export default class RapidProWebhook implements IWebhookRepository {
 
     constructor(
+        private readonly read: IRead,
         private readonly http: IHttp,
         private readonly baseCallbackUrl: string,
         private readonly secret: string,
-    ) {}
+    ) { }
 
     public async onCloseRoom(room: Room): Promise<void> {
         const payload = {
@@ -30,6 +33,37 @@ export default class RapidProWebhook implements IWebhookRepository {
         await this.http.post(this.baseCallbackUrl + '/closeRoom', reqOptions);
     }
 
+    public async sendAgentMessage(room: Room, text?: string, attachments?: Array<IMessageAttachment>): Promise<void> {
+        const payload = {
+            type: 'agent-message',
+            ticketId: room.ticketId,
+            visitor: {
+                token: room.room.visitor.token,
+            },
+            data: {
+                agent: {
+                    id: room.room.servedBy!.id,
+                },
+                text,
+            },
+        };
+
+        if (attachments) {
+            const formattedAttachments: { [key: string]: any } = [];
+            await Promise.all(attachments.map(async (attachment) => {
+                const type = this.getAttachmentType(attachment);
+                const url = await this.buildAttachmentUrl(attachment);
+                formattedAttachments.push({ type, url });
+            }));
+            payload.data['attachments'] = formattedAttachments;
+        }
+
+        const reqOptions = this.requestOptions();
+        reqOptions['data'] = payload;
+
+        await this.http.post(this.baseCallbackUrl + '/closeRoom', reqOptions);
+    }
+
     private requestOptions(): object {
         return {
             headers: {
@@ -37,6 +71,17 @@ export default class RapidProWebhook implements IWebhookRepository {
                 'Authorization': `Token ${this.secret}`,
             },
         };
+    }
+
+    private getAttachmentType(attachment: IMessageAttachment): string {
+        return attachment['videoType'] || attachment['audioType'] || attachment['imageType'] || 'document';
+    }
+
+    private async buildAttachmentUrl(attachment: IMessageAttachment): Promise<string> {
+
+        const siteUrl = await this.read.getEnvironmentReader().getServerSettings().getValueById(RC_SERVER_URL);
+        return `${siteUrl}${attachment.title!.link}`;
+
     }
 
 }
